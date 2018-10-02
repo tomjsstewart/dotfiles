57dcc404b479650573b7d299f256dcab8a72d2d2
Tom Stewart <T.Stewart.4@warwick.ac.uk>
2018-10-02 16:34:10 +0100

    Add google search option

---
+++ web.sh
@@ -0,0 +1,13 @@
+function chrome-dot() {
+    URL=$1
+    if [[ $1 != http* ]] ; then
+        URL="http://$1"
+    fi
+    chrome "$URL"
+}
+
+
+function search() {
+    QUERY=$1
+    chrome-dot "https://www.google.co.uk/search?q=$QUERY&ie=UTF-8&oe=UTF-8"
+}
