b1bdd007c29c20662abbd9f334bf5c69bc048146
tomjsstewart <tomjsstewart@users.noreply.github.com>
2020-02-06 20:49:32 +0000

    Add eval $(ssh-agent)
    Don't need ssh passphrase everytime

--- git.sh
+++ git.sh
@@ -10,3 +10,7 @@ alias gl="git log --oneline --all --graph --decorate  $*"
 
 alias status="git status"
 alias s="git status"
+
+
+# ssh keys need this to not require passphrase everytime
+eval $(ssh-agent)
